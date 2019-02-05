20a16a2209af01416cab73369fd55fe32d39e762
Mitchell Pleune <mitchpleune@gmail.com>
2019-02-05 15:32:53 -0500

    Add state publisher

--- estop.cpp
+++ estop.cpp
@@ -1,36 +1,73 @@
 #include <cstdlib>
 #include <geometry_msgs/Twist.h>
 #include <ros/ros.h>
+#include <std_msgs/Bool.h>
 #include <std_srvs/Empty.h>
 #include <std_srvs/Trigger.h>
 #include <string>
 
-#define ZEROS_HZ 50
-
-enum EstopState { RUNNING, STOPPED } static state = STOPPED;
-
 // Only one node is allowed to call resume and succeed (the first one to call).
 // The eStop node starts in the stopped state, so the node that initially
 // resumes must be the same node that does it in the future.
-static std::string resume_node;
 
-// Output publisher
-static ros::Publisher pub;
+#define ZEROS_HZ 50
+#define STATE_HZ 0.5
 
-// publish zeros when enabled
-static ros::Timer zeros;
+enum EstopState { RUNNING, STOPPED } static state = STOPPED;
+
+static bool stop_cb(ros::ServiceEvent<std_srvs::EmptyRequest, std_srvs::EmptyResponse> &event);
+static bool resume_cb(ros::ServiceEvent<std_srvs::TriggerRequest, std_srvs::TriggerResponse> &event);
+static void forwarded_cb(const geometry_msgs::Twist::ConstPtr &msg);
+static void publish_zero_cb(const ros::TimerEvent);
+static void publish_state_cb(const ros::TimerEvent);
+
+static std::string resume_node; // node name with resume permission
+static ros::Publisher pub_twist; // Output Twist publisher
+static ros::Publisher pub_state; // Periodic state publisher
+static ros::Timer zeros; // publish zeros when enabled
+
+int
+main(int argc, char **argv)
+{
+    ros::init(argc, argv, "eStop");
+    ros::NodeHandle nh("~");
+
+    std::string input_topic, output_topic;
+
+    if (!nh.getParam("input_topic", input_topic))
+    {
+        ROS_ERROR_STREAM("eStop forwarded topic not specified");
+        return EXIT_FAILURE;
+    }
+
+    if (!nh.getParam("output_topic", output_topic)) output_topic = "out";
+
+    pub_twist = nh.advertise<geometry_msgs::Twist>(output_topic, 1);
+    pub_state = nh.advertise<std_msgs::Bool>("state", 1);
+    ros::Subscriber forwarded = nh.subscribe(input_topic, 1, &forwarded_cb);
+    ros::ServiceServer srv_stop = nh.advertiseService("stop", &stop_cb);
+    ros::ServiceServer srv_restart = nh.advertiseService("resume", &resume_cb);
+    zeros = nh.createTimer(ros::Duration(1.0 / ZEROS_HZ), &publish_zero_cb);
+
+    ros::Timer state = nh.createTimer(ros::Duration(1.0 / STATE_HZ), &publish_state_cb);
+
+    ros::spin();
+    return EXIT_SUCCESS;
+}
 
 static bool
-stop(ros::ServiceEvent<std_srvs::EmptyRequest, std_srvs::EmptyResponse> &event)
+stop_cb(ros::ServiceEvent<std_srvs::EmptyRequest, std_srvs::EmptyResponse> &event)
 {
     state = STOPPED;
     zeros.start();
+    std_msgs::Bool msg;
+    msg.data = true;
+    pub_state.publish(msg);
     return true;
 }
 
 static bool
-resume(ros::ServiceEvent<std_srvs::TriggerRequest, std_srvs::TriggerResponse>
-           &event)
+resume_cb(ros::ServiceEvent<std_srvs::TriggerRequest, std_srvs::TriggerResponse> &event)
 {
     const std::string &caller = event.getCallerName();
     if (resume_node.empty()) resume_node = caller;
@@ -49,17 +86,20 @@ resume(ros::ServiceEvent<std_srvs::TriggerRequest, std_srvs::TriggerResponse>
         response.success = true;
     }
 
+    std_msgs::Bool msg;
+    msg.data = (state == STOPPED);
+    pub_state.publish(msg);
     return true;
 }
 
 static void
 forwarded_cb(const geometry_msgs::Twist::ConstPtr &msg)
 {
-    if (state == RUNNING) pub.publish(msg);
+    if (state == RUNNING) pub_twist.publish(msg);
 }
 
-void
-publish_zero(const ros::TimerEvent)
+static void
+publish_zero_cb(const ros::TimerEvent)
 {
     geometry_msgs::Twist msg;
     msg.angular.x = 0;
@@ -68,32 +108,13 @@ publish_zero(const ros::TimerEvent)
     msg.linear.x = 0;
     msg.linear.y = 0;
     msg.linear.z = 0;
-    pub.publish(msg);
+    pub_twist.publish(msg);
 }
 
-int
-main(int argc, char **argv)
+static void
+publish_state_cb(const ros::TimerEvent)
 {
-    ros::init(argc, argv, "eStop");
-    ros::NodeHandle nh("~");
-
-    std::string input_topic, output_topic;
-
-    if (!nh.getParam("input_topic", input_topic))
-    {
-        ROS_ERROR_STREAM("eStop forwarded topic not specified");
-        return EXIT_FAILURE;
-    }
-
-    if (!nh.getParam("output_topic", output_topic))
-        output_topic = "out";
-
-    pub = nh.advertise<geometry_msgs::Twist>(output_topic, 1);
-    ros::Subscriber forwarded = nh.subscribe(input_topic, 1, &forwarded_cb);
-    ros::ServiceServer srv_stop = nh.advertiseService("stop", &stop);
-    ros::ServiceServer srv_restart = nh.advertiseService("resume", &resume);
-    zeros = nh.createTimer(ros::Duration(1.0 / ZEROS_HZ), &publish_zero);
-
-    ros::spin();
-    return EXIT_SUCCESS;
+    std_msgs::Bool msg;
+    msg.data = (state == STOPPED);
+    pub_state.publish(msg);
 }
